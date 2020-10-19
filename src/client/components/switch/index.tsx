53aed3190bf0d0236ab7c0267eda91c5b3b8c388
GoltsovS <firs1221@mail.ru>
2020-10-19 12:03:38 +0300

    add switch on/off indicator onclick label handler add switch local state

--- src/client/components/switch/index.tsx
+++ src/client/components/switch/index.tsx
@@ -1,19 +1,34 @@
 import * as React from 'react';
-import { FC, ReactElement } from 'react';
+import { FC, ReactElement, MouseEvent, useState } from 'react';
 import './styles.styl';
 
 export interface SwitchProps {
   className?: string;
   id: string;
   label: string;
+  onClick?: (event: MouseEvent) => void;
 }
+
 const Switch: FC<SwitchProps> = (props): ReactElement => {
-  const { className, id, label } = props;
+  const { className, id, label, onClick } = props;
+  const [isChecked, toggleChecked] = useState<boolean>(false);
+  const handleClick = (event: MouseEvent) => {
+    if (onClick) onClick(event);
+    toggleChecked(!isChecked);
+  };
   return (
     <div className={`switch ${className ? className : ''}`}>
-      <span>{label}</span>
-      <input type="checkbox" id={id} />
-      <label htmlFor={id}></label>
+      <span>
+        {label} : {isChecked ? 'вкл' : 'выкл'}
+      </span>
+      <input type="checkbox" id={id} checked={isChecked} />
+      <label
+        htmlFor={id}
+        onClick={(event) => {
+          event.preventDefault();
+          handleClick(event);
+        }}
+      />
     </div>
   );
 };
