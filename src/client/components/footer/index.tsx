4dbcafa3b41bc7f01de0fcfe46e73bd9f9de1520
GoltsovS <firs1221@mail.ru>
2020-12-27 16:15:49 +0300

    update footer template update footer styles

--- src/client/components/footer/index.tsx
+++ src/client/components/footer/index.tsx
@@ -6,12 +6,14 @@ import './styles.styl';
 const Footer: FC = (): ReactElement => {
   return (
     <footer className="footer">
-      <section className="footer__col">
-        <span className="footer__copyright">Все права защищены © 2020 CadrInc</span>
-      </section>
-      <section className="footer__col">
-        <Logo width='90px' height='20px'/>
-      </section>
+      <div className="footer__container width-container">
+        <section className="footer__col">
+          <span className="footer__copyright">Все права защищены © 2020 CadrInc</span>
+        </section>
+        <section className="footer__col">
+          <Logo width='90px' height='20px'/>
+        </section>
+      </div>
     </footer>
   );
 };
